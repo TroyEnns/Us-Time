609c88afa93f8c6dbaf4f710f2556cb2f86182f6
Troy Enns <TroyEnns@gmail.com>
2016-03-15 10:55:23 -0700

    finish most of html and css

---
+++ script.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+	var movieGenres=[
+
+					];
+
+	function foodIn(){
+		$("#decisions").fadeOut("slow", function(){
+			$("#food").fadeIn("slow");
+		});
+	}
+	function moviesIn(){
+		$("#decisions").fadeOut("slow", function(){
+			$("#movies").fadeIn("slow");
+		});
+	}
+
+	function getFood(){
+		$.ajax({
+        url:"https://api.yelp.com/v2/search?term=food&location=San+Francisco&limit=3",
+        jsonp: "callback",
+        dataType: "jsonp",
+        oauth_consumer_key: "Q5sozFRWW3WUJhSXc3kwSw",
+        oauth_token:"dwZARp07lKGEcUf4dSt2zlrSxMWdEDdw",
+        oauth_signature_method:"hmac-sha1",
+        oauth_signature:"fmZdqzrk4xVC-J6-ZOjCo8HtD-E"
+    	}).done(function(data){
+        console.log(data)
+        });
+	}
+
+	function getMovie(){
+		$.ajax({
+        url:"http://netflixroulette.net/api/api.php?title=Attack%20on%20titan",
+        json: "callback",
+        dataType: "json"
+      }).done(function(data){
+        console.log(data);
+    });
+	}
+
+	$("#foodButton").on("click",function(){
+		foodIn();
+		getFood();
+	});
+
+	$("#movieButton").on("click",function(){
+		moviesIn();
+		getMovie();
+	});
+
+
+});
